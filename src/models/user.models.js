d453937fa5e4c34f7b1d9156b409db88f4490392
prabuddhaxdev <prabuddha1749@gmail.com>
2026-04-27 01:24:59 +0530

    feat: created user schema using mongoose

---
+++ src/models/user.models.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.schema({
+    username:{
+        type: String,
+        unique: [true,"Username already exists"],
+        required: [true,"Username is required"]
+    },
+    email:{
+        type:String,
+        unique: [true,"Email already exists"],
+        required: [true,"Email is required"]
+    },
+    password:{
+        type: String,
+        required: [true,"Password is required"]
+    },
+    bio:String,
+    profileImage:{
+        type: String,
+        default:"https://ik.imagekit.io/60kfj44cw/default-user-image.avif"
+    }
+})
+
+const userModel = mongoose.model("users",userSchema)
+
+module.exports = userModel
