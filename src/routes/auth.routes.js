b99ba15aa042d3a50e20aa3eea9786463650670c
prabuddhaxdev <prabuddha1749@gmail.com>
2026-04-27 23:55:56 +0530

    Add register auth route

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const userModel = require('../models/user.models');
+
+const authRouter = express.Router();
+
+authRouter.post('/register', async(req,res) => {
+    const { email, username, password, bio, profileImage} = req.body
+
+    const isUserExistsByEmail = await userModel.findOne({ email })
+
+    if(isUserExistsByEmail){
+        return res.status(409).json({
+            message:"User already exists with same email"
+        }
+        )
+    }
+
+    const isUserExistsByUsername = await userModel.findOne({ username });
+
+    if (isUserExistsByUsername) {
+      return res.status(409).json({
+        message: "User already exists with same username",
+      });
+    }
+
+})
