b0fd7dedce0e7771347542dcfe5131fccb5fe2dc
prabuddhaxdev <prabuddha1749@gmail.com>
2026-04-28 13:21:17 +0530

    feat: created user register api using JWT

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,26 +1,52 @@
 const express = require('express');
 const userModel = require('../models/user.models');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const { profile } = require('console');
 
 const authRouter = express.Router();
 
+
+
+// POST  /api/auth/register
 authRouter.post('/register', async(req,res) => {
     const { email, username, password, bio, profileImage} = req.body
 
-    const isUserExistsByEmail = await userModel.findOne({ email })
+    const isUserAlreadyExists = await userModel.findOne({
+        $or: [
+            { username}, { email }
+        ]
+     })
 
-    if(isUserExistsByEmail){
+    if(isUserAlreadyExists){
         return res.status(409).json({
-            message:"User already exists with same email"
+            message:"User already exists " + (isUserAlreadyExists.email == email ? "Email already exists " : "Username already exists ")}
+    )}
+
+    const hash = crypto.createHash('sha256').update(password).digest('hex')
+
+    const user = await userModel.create({
+        username, email, bio, profileImage,
+        password:hash
+    })
+
+    const token = jwt.sign(
+        {
+            id: user._id
+        },
+        process.env.JWT_SECRET,{ expiresIn : "1d" }
+    )
+    res.cookie("token", token)
+
+    res.status(201).json({
+        message: "User registered successfully",
+        user:{
+            email: user.email,
+            username: user.username,
+            bio: user.bio,
+            profileImage: user.profileImage
         }
-        )
-    }
-
-    const isUserExistsByUsername = await userModel.findOne({ username });
-
-    if (isUserExistsByUsername) {
-      return res.status(409).json({
-        message: "User already exists with same username",
-      });
-    }
-
+    })
 })
+
+module.exports = authRouter
