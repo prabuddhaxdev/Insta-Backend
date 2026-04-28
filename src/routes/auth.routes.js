6b6583bbdc8e48f392f84a8ec9b1548d44bfdbdc
prabuddhaxdev <prabuddha1749@gmail.com>
2026-04-28 15:09:33 +0530

    feat: created auth controllers and separated the login and register logic from the routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,102 +1,13 @@
 const express = require('express');
-const userModel = require('../models/user.models');
-const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
-const { profile } = require('console');
+const authController = require("../controllers/auth.controllers")
 
 const authRouter = express.Router();
 
 
-
 // POST  /api/auth/register
-authRouter.post('/register', async(req,res) => {
-    const { email, username, password, bio, profileImage} = req.body
-
-    const isUserAlreadyExists = await userModel.findOne({
-        $or: [
-            { username}, { email }
-        ]
-     })
-
-    if(isUserAlreadyExists){
-        return res.status(409).json({
-            message:"User already exists " + (isUserAlreadyExists.email == email ? "Email already exists " : "Username already exists ")}
-    )}
-
-    const hash = crypto.createHash('sha256').update(password).digest('hex')
-
-    const user = await userModel.create({
-        username, email, bio, profileImage,
-        password:hash
-    })
-
-    const token = jwt.sign(
-        {
-            id: user._id
-        },
-        process.env.JWT_SECRET,{ expiresIn : "1d" }
-    )
-    res.cookie("token", token)
-
-    res.status(201).json({
-        message: "User registered successfully",
-        user:{
-            email: user.email,
-            username: user.username,
-            bio: user.bio,
-            profileImage: user.profileImage
-        }
-    })
-})
+authRouter.post('/register', authController.registerController )
 
 // POST  /api/auth/login
-authRouter.post("/login", async(req, res) => {
-    const { username, email, password} = req.body;
-
-    const user = await userModel.findOne({
-        $or:[{
-            username: username
-        },
-        {
-            email: email
-        }
-        ]
-    })
-
-    if(!user){
-        return res.status(404).json({
-            message: "User not found"
-        })
-    }
-
-     const hash = crypto.createHash("sha256").update(password).digest("hex");
-
-     const isPasswordValid = hash == user.password
-
-     if(!isPasswordValid){
-        return res.status(401).json({
-            message: "Password invalid"
-        })
-     }
-
-     const token = jwt.sign(
-        {id: user._id},
-        process.env.JWT_SECRET,
-        {expiresIn: "1d"}
-     )
-
-     res.cookie("token", token);
-
-     res.status(200).json({
-        message: "User logged in successfully",
-        user:{
-            username: user.username,
-            email: user.email,
-            bio: user.bio,
-            profileImage: user.profileImage
-        }
-     })
-
-})
+authRouter.post("/login", authController.loginController)
 
 module.exports = authRouter
