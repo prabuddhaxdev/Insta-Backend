0541be29c97e12f74c0f8b37e66191cbe0031119
prabuddhaxdev <prabuddha1749@gmail.com>
2026-04-28 15:00:49 +0530

    feat: created user login api

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -49,4 +49,54 @@ authRouter.post('/register', async(req,res) => {
     })
 })
 
+// POST  /api/auth/login
+authRouter.post("/login", async(req, res) => {
+    const { username, email, password} = req.body;
+
+    const user = await userModel.findOne({
+        $or:[{
+            username: username
+        },
+        {
+            email: email
+        }
+        ]
+    })
+
+    if(!user){
+        return res.status(404).json({
+            message: "User not found"
+        })
+    }
+
+     const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+     const isPasswordValid = hash == user.password
+
+     if(!isPasswordValid){
+        return res.status(401).json({
+            message: "Password invalid"
+        })
+     }
+
+     const token = jwt.sign(
+        {id: user._id},
+        process.env.JWT_SECRET,
+        {expiresIn: "1d"}
+     )
+
+     res.cookie("token", token);
+
+     res.status(200).json({
+        message: "User logged in successfully",
+        user:{
+            username: user.username,
+            email: user.email,
+            bio: user.bio,
+            profileImage: user.profileImage
+        }
+     })
+
+})
+
 module.exports = authRouter
