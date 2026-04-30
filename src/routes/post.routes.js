eff059455c1144635ffa3fffd864f6cc59033df5
prabuddhaxdev <prabuddha1749@gmail.com>
2026-04-30 23:34:27 +0530

    feat: add imagekit for storing files and also fixed multer import

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const postRouter = express.Router();
 const postController = require("../controllers/post.controllers");
-const multer = require("./multer");
+const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 
 // POST  /api/posts/
