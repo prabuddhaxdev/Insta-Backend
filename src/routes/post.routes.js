84a4af9c176950ef34de3b9dd99a6366b8f971c8
prabuddhaxdev <prabuddha1749@gmail.com>
2026-05-12 01:56:54 +0530

    feat: add getPost and getPostDetails controllers for fetching user posts

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -8,4 +8,10 @@ const upload = multer({ storage: multer.memoryStorage() });
 postRouter.post("/", upload.single("image"), postController.createPostController
 );
 
+// GET /api/posts/
+postRouter.get("/", postController.getPostController)
+
+//GET /api/posts/details/:postid
+postRouter.get("/details/:postid", postController.getPostDetailsController);
+
 module.exports = postRouter;
