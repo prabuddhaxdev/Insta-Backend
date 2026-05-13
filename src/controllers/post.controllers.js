7d85981f4f5a1d05e7105efb61f3f8809671a5c6
prabuddhaxdev <prabuddha1749@gmail.com>
2026-05-13 10:37:58 +0530

    feat: refactor post model structure and add follow model with follower and followee references

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -1,4 +1,4 @@
-const postModel = require('../models/post.model')
+const postModel = require('../models/post.models')
 const Imagekit = require('@imagekit/nodejs')
 const { toFile } = require('@imagekit/nodejs')
 const jwt = require('jsonwebtoken')
