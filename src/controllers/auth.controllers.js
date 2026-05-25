c2eb86fc7a344e1950e4ea3b834369998de81f8e
prabuddhaxdev <prabuddha1749@gmail.com>
2026-05-25 14:54:08 +0530

    feat: add user routes and controllers, enhance JWT payload with username

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -32,6 +32,7 @@ async function registerController(req, res){
   const token = jwt.sign(
     {
       id: user._id,
+      username: user.username,
     },
     process.env.JWT_SECRET,
     { expiresIn: "1d" },
@@ -77,9 +78,13 @@ async function loginController(req, res) {
     });
   }
 
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
+  const token = jwt.sign(
+    { id: user._id, username: user.username },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    },
+  );
 
   res.cookie("token", token);
 
---
+++ src/routes/user.routes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+const useRouter = express.Router();
+
+module.exports = useRouter;
