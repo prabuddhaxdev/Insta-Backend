10d8e0186f67cf715b7860f30cb79f0398d52122
prabuddhaxdev <prabuddha1749@gmail.com>
2026-05-26 00:26:02 +0530

    feat: add follow and unfollow routes with user authentication

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,29 @@
-const express = require('express');
+const express = require("express");
+const userController = require("../controllers/user.controllers");
+const identifyUser = require("../middlewares/auth.middlewares");
 
-const useRouter = express.Router();
+const userRouter = express.Router();
 
-module.exports = useRouter;
+/**
+ * @route POST /api/users/follow/:userid
+ * @description Follow a user
+ * @access Private
+ */
+userRouter.post(
+  "/follow/:username",
+  identifyUser,
+  userController.followUserController,
+);
+
+/**
+ * @route POST /api/users/unfollow/:userid
+ * @description Unfollow a user
+ * @access Private
+ */
+userRouter.post(
+  "/unfollow/:username",
+  identifyUser,
+  userController.unfollowUserController,
+);
+
+module.exports = userRouter;
