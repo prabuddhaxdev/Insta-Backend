d7960fe50fe9ce52efc669a4e4d95e6aae6f9306
prabuddhaxdev <prabuddha1749@gmail.com>
2026-07-03 20:48:02 +0530

    feat: refactor routing and authentication context, add global styles and button styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import AppRoutes from "./AppRoutes";
+import { RouterProvider } from "react-router";
+import { router } from "./AppRoutes";
 import { AuthProvider } from "./features/auth/auth.context";
+
 
 function App() {
   return (
     <AuthProvider>
-      <AppRoutes />
+      <RouterProvider router={router} />
     </AuthProvider>
   );
 }
--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -1,15 +1,18 @@
-import{ BrowserRouter, Route, Routes } from 'react-router'
-import Login from './features/auth/pages/Login'
-import Register from './features/auth/pages/Register'
+import { createBrowserRouter } from "react-router";
+import Login from "./features/auth/pages/Login";
+import Register from "./features/auth/pages/Register";
 
-export default function AppRoutes(){
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<h1>Welcome to the App</h1>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </BrowserRouter>
-    );
-}
+export const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/",
+    element: <h1>Welcome to 4 layer architecture of react</h1>,
+  },
+]);
